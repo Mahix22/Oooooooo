7a7f88506d159ee7f7ff0ca98a695288d13b22f6
Mahix22 <seyamahmed240@gmail.com>
2024-06-19 21:24:06 +0600

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,43 +1,66 @@
-const axios = require('axios');
-
+const { getPrefix, getStreamFromURL, uploadImgbb } = global.utils;
+async function ai({ message: m, event: e, args: a, usersData: u }) {
+  var p = [`${await getPrefix(e.threadID)}${this.config.name}`,
+`${this.config.name}`
+/*"ai"
+*you can add more prefix here
+*/
+]; 
+ if (p.some(b => a[0].toLowerCase().startsWith(b))) {
+try {      
+let prompt = "";
+if (e.type === "message_reply" && e.messageReply.attachments && e.messageReply.attachments[0]?.type === "photo") {
+ const b = await uploadImgbb(e.messageReply.attachments[0].url);
+prompt = a.slice(1).join(" ") + ' ' + b.image.url;
+} else {
+ prompt = a.slice(1).join(" ");
+}
+ var __ = [{ id: e.senderID, tag: await u.getName(e.senderID) }];
+ const r = await require("axios").post(`https://test-ai-ihc6.onrender.com/api`, {
+  prompt: prompt,
+ apikey: "GayKey-oWHmMb1t8ASljhpgSSUI",
+  name: __[0]['tag'],
+ id: __[0]['id'],
+ });
+var _ = r.data.result.replace(/{name}/g, __[0]['tag']).replace(/{pn}/g, p[0]);
+ if (r.data.av) {
+ if (Array.isArray(r.data.av)) {
+ const avs = r.data.av.map(url => getStreamFromURL(url));
+ const avss = await Promise.all(avs);
+  m.reply({
+ body: _,
+ mentions: __,
+ attachment: avss
+ });
+ } else {
+ m.reply({
+ body: _,
+ mentions: __,
+attachment: await getStreamFromURL(r.data.av)
+  });
+  }
+  } else {
+m.reply({
+body: _,
+mentions: __
+  });
+  }
+  } catch (error) {
+ m.reply("Error " + error);
+ }
+ }
+}
 module.exports = {
-  config: {
-    name: "ai",
-
-
-    author: "ᴍʀ.ᴀʏᴀɴ",
-    version: "2.0",
-    cooldowns: 0,
-    role: 0,
-    shortDescription: {
-      en: "ai cmd powered by blue",
-    },
-    category: "ai",
-    guide: {
-      en: "blufuj [your content]",
-    },
-  },
-
-  onStart: async function ({ api, event, args }) {
-    const content = encodeURIComponent(args.join(" "));
-
-    if (!content) {
-      return api.sendMessage("⚠️ | ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜʀ ᴀsᴋ...", event.threadID, event.messageID);
-    }
-
-    api.sendMessage("📩 | ᴡᴀɪᴛ ʙᴀʙᴜ ᴀɪ ʟᴏᴅɪɴɢ...", event.threadID, event.messageID);
-
-    const apiUrl = `https://bluerepoapislasttry.onrender.com/hercai?content=${content}`;
-
-    try {
-      const response = await axios.get(apiUrl);
-      const reply = response.data.reply;
-
-      api.sendMessage(reply, event.threadID, event.messageID);
-    } catch (error) {
-      console.error("Error fetching data:", error.message);
-      console.error("Error details:", error.response ? error.response.data : error.message);
-      api.sendMessage("An error occurred while processing your request. Check the server logs for more details.", event.threadID);
-    }
-  },
+config: {
+ name: "ai",
+aliases: "",
+version: 1.6,
+author: "Jun",
+role: 0,
+ shortDescription: "An AI that can do various tasks",
+ guide: "{pn} <query>",
+ category: "AI"
+ },
+ onStart: function() {},
+ onChat: ai
 };
