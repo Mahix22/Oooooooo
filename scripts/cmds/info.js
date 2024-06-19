51e625a568eea35e4c54247713b9589dd1069afd
Mahix22 <seyamahmed240@gmail.com>
2024-06-19 22:01:31 +0600

    Create info.js

---
+++ scripts/cmds/info.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const moment = require('moment-timezone');
+
+module.exports = {
+  config: {
+    name: "info",
+    aliases: ["admininfo", "owner"],
+    version: "2.0",
+    author: "RUBISH",
+    countDown: 5,
+    role: 0,
+    shortDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin along with an image."
+    },
+    longDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin along with an image."
+    },
+    category: "Information",
+    guide: {
+      en: "{pn}"
+    },
+    envConfig: {}
+  },
+
+  onStart: async function ({ message }) {
+    this.sendInfo(message);
+  },
+
+  onChat: async function ({ event, message }) {
+    if (event.body && event.body.toLowerCase() === "info") {
+      this.sendInfo(message);
+    }
+  },
+
+  sendInfo: async function (message) {
+    const botName = "Spriha v2🌸";
+    const botPrefix = ".";
+    const authorName = "MAHI AHMED";
+    const authorFB = "https://www.facebook.com/profile.php?id=100089286199594";
+    const authorInsta = "secret";
+    const status = "Pure single";
+
+    const urls = JSON.parse(fs.readFileSync('Rubish.json'));
+    const link = urls[Math.floor(Math.random() * urls.length)];
+
+    const now = moment().tz('Asia/Dhaka');
+    const date = now.format('MMMM Do YYYY');
+    const time = now.format('h:mm:ss A');
+
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `${hours}h ${minutes}m ${seconds}sec`;
+
+    message.reply({
+      body: `
+≡≡║Bot & Owner Info║≡≡
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+
+➠Bot Name↠ ${botName}
+
+➠Bot Prefix↠ ${botPrefix}
+
+➠Owner Name↠ ${authorName}
+
+➠Facebook↠ ${authorFB}
+
+➠Instagram↠ ${authorInsta}
+
+➠Status↠ ${status}
+
+➠Date↠ ${date}
+
+➠Time↠ ${time}
+
+➠Uptime↠ ${uptimeString}
+
+﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
+Thanks for using ↠ \n${botName}
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏`,
+      attachment: await global.utils.getStreamFromURL(link)
+    });
+  }
+};
