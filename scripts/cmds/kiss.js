eb27a291dbf16b433f5e2afc4109e5812792da8e
Mahix22 <seyamahmed240@gmail.com>
2024-06-19 22:12:30 +0600

    Update kiss.js

--- scripts/cmds/kiss.js
+++ scripts/cmds/kiss.js
@@ -1,55 +1,36 @@
-const axios = require ("axios");
-const fs = require ("fs-extra");
-
+const DIG = require("discord-image-generation");
+const fs = require("fs-extra");
+const Discord = require ("discord.js")
+const request = require ("node-superfetch");
 module.exports = {
-  config: {
-    name: "kiss",
-    aliases: [],
-    version: "1.0",
-    author: "ᴍʀ.ᴀʏᴀɴ",
-    countDown: 5,
-    role: 0,
-    shortDescription: " ",
-    longDescription: "",
-    category: "love",
-    guide: "{pn}"
-  },
+	config: {
+		name: "kiss",
+		version: "1.1",
+		author: "jun",
+		countDown: 5,
+		role: 0,
+		shortDescription: "kiss",
+		longDescription: "kiss",
+		category: "image",
+		guide: {
+			en: "   {pn} @tag"
+		}
+	},
 
-  onStart: async function({ api, event, threadsData, usersData }) {
-
-    const { threadID, messageID, senderID } = event;
-    const { participantIDs } = await api.getThreadInfo(threadID);
-    var tle = Math.floor(Math.random() * 101);
-    var namee = (await usersData.get(senderID)).name
-    const botID = api.getCurrentUserID();
-    const listUserID = participantIDs.filter(ID => ID != botID && ID != senderID);
-    var id = listUserID[Math.floor(Math.random() * listUserID.length)];
-    var name = (await usersData.get(id)).name
-    var arraytag = [];
-    arraytag.push({ id: senderID, tag: namee });
-    arraytag.push({ id: id, tag: name });
-
-    let Avatar = (await axios.get(`https://graph.facebook.com/${senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8"));
-
-    let gifLove = (await axios.get(`https://i.imgur.com/Ef5Ftnl.gif`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8"));
-
-    let Avatar2 = (await axios.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8"));
-
-    var imglove = [];
-
-    imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-    imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-    imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
-
-    var msg = {
-      body: `💋sᴜᴄᴄᴇssғᴜʟ ᴋɪssɪɴɢ!\n🤭ᴡɪsʜ ʏᴏᴜ ᴛᴡᴏ ʜᴜɴᴅʀᴇᴅ ʏᴇᴀʀs ᴏғ ʜᴀᴘᴘɪɴᴇss\n💕ᴅᴏᴜʙʟᴇ ʀᴀᴛɪᴏ: ${tle}%\n${namee} 💓 ${name}`,
-      mentions: arraytag,
-      attachment: imglove
-    };
-
-    return api.sendMessage(msg, event.threadID, event.messageID);
-  }
+onStart: async function ({ event, message, usersData, args, getLang }) {
+		const uid1 = event.senderID;
+		const uid2 = Object.keys(event.mentions)[0];
+		if (!uid2)
+			return message.reply(getLang("noTag"));
+		const avatarURL1 = await usersData.getAvatarUrl(uid1);
+		const avatarURL2 = await usersData.getAvatarUrl(uid2);
+	const img = await new DIG.Kiss().getImage(avatarURL1, avatarURL2);
+		const pathSave = `${__dirname}/tmp/${uid1}_${uid2}Kiss.png`;
+		fs.writeFileSync(pathSave, Buffer.from(img));
+		const content = args.join(' ').replace(Object.keys(event.mentions)[0], "");
+		message.reply({
+			body: `${(content || "ummah baby😘😘")}`,
+			attachment: fs.createReadStream(pathSave)
+		}, () => fs.unlinkSync(pathSave));
+	}
 };
