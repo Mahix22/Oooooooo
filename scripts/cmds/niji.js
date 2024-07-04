eb498ec41b0bf16845104d1ad95fd27008d4f194
Mahix22 <seyamahmed240@gmail.com>
2024-07-05 04:46:22 +0600

    Update niji.js

--- scripts/cmds/niji.js
+++ scripts/cmds/niji.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
-
+ 
 module.exports = {
   config: {
     name: "niji",
@@ -21,7 +21,7 @@ module.exports = {
       en: "[userprompt]"
     }
   },
-
+ 
   generateImage: async function(prompt) {
     const apiUrl = `https://api.vyturex.com/niji?text=${encodeURIComponent(prompt)}`;
     try {
@@ -36,7 +36,7 @@ module.exports = {
       throw error;
     }
   },
-
+ 
   downloadImage: async function(url, filePath) {
     const writer = fs.createWriteStream(filePath);
     const response = await axios({
@@ -44,25 +44,25 @@ module.exports = {
       method: 'GET',
       responseType: 'stream'
     });
-
+ 
     response.data.pipe(writer);
-
+ 
     return new Promise((resolve, reject) => {
       writer.on('finish', resolve);
       writer.on('error', reject);
     });
   },
-
+ 
   onStart: async function ({ api, event, args }) {
     const userPrompt = args.join(" ");
     const waitingMessage = await api.sendMessage("Generating your image, please wait...", event.threadID);
-
+ 
     try {
       const imageUrl = await module.exports.generateImage(userPrompt);
       const imagePath = path.join(__dirname, 'generated_image.png');
-
+ 
       await module.exports.downloadImage(imageUrl, imagePath);
-
+ 
       const message = {
         body: `Here is the generated image for your prompt: ${userPrompt}`,
         attachment: fs.createReadStream(imagePath)
