cc9c76e482c06a4ed71e2f400bea3230ac8fe2af
Mahix22 <seyamahmed240@gmail.com>
2024-07-07 20:42:04 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -1,88 +1,43 @@
-const fs = require('fs');
-const moment = require('moment-timezone');
-
+const { getStreamFromURL } = require("fb-watchman");
 module.exports = {
   config: {
     name: "info",
-    aliases: ["admininfo", "owner"],
-    version: "2.0",
-    author: "RUBISH",
-    countDown: 5,
-    role: 0,
-    shortDescription: {
-      vi: "",
-      en: "Sends information about the bot and admin along with an image."
-    },
-    longDescription: {
-      vi: "",
-      en: "Sends information about the bot and admin along with an image."
-    },
-    category: "Information",
+    version: 2.0,
+    author: "OtinXSandip",
+    longDescription: "info about bot and owner",
+    category: "ai",
     guide: {
-      en: "{pn}"
+      en: "{p}{n}",
     },
-    envConfig: {}
   },
 
-  onStart: async function ({ message }) {
-    this.sendInfo(message);
-  },
+  onStart: async function ({ api, event, args, message, usersData }) {
+    const imgURL = "https://i.imgur.com/gMvwoja.jpeg";
+    const attachment = await global.utils.getStreamFromURL(imgURL);
 
-  onChat: async function ({ event, message }) {
-    if (event.body && event.body.toLowerCase() === "info") {
-      this.sendInfo(message);
-    }
-  },
+    const id = event.senderID;
+    const userData = await usersData.get(id);
+    const name = userData.name;
 
-  sendInfo: async function (message) {
-    const botName = "Spriha v2🌸";
-    const botPrefix = ".";
-    const authorName = "MAHI AHMED";
-    const authorFB = "https://www.facebook.com/profile.php?id=100089286199594";
-    const authorInsta = "secret";
-    const status = "Pure single";
+    const ment = [{ id: id, tag: name }];
+    const a = "SPRIHA V2 🕊🤍";
+    const b = ".";
+    const c = "MAHI AHMED ♡♡";
+const e = "Male";
+    const d = "https://www.facebook.com/profile.php?id=100089286199594";
+const f = " DARKNESS";
+const g = "None 💀";
 
-    const urls = JSON.parse(fs.readFileSync('Rubish.json'));
-    const link = urls[Math.floor(Math.random() * urls.length)];
-
-    const now = moment().tz('Asia/Dhaka');
-    const date = now.format('MMMM Do YYYY');
-    const time = now.format('h:mm:ss A');
-
-    const uptime = process.uptime();
-    const seconds = Math.floor(uptime % 60);
-    const minutes = Math.floor((uptime / 60) % 60);
-    const hours = Math.floor((uptime / (60 * 60)) % 24);
-    const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `${hours}h ${minutes}m ${seconds}sec`;
-
-    message.reply({
-      body: `
-≡≡║Bot & Owner Info║≡≡
-﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
-
-➠Bot Name↠ ${botName}
-
-➠Bot Prefix↠ ${botPrefix}
-
-➠Owner Name↠ ${authorName}
-
-➠Facebook↠ ${authorFB}
-
-➠Instagram↠ ${authorInsta}
-
-➠Status↠ ${status}
-
-➠Date↠ ${date}
-
-➠Time↠ ${time}
-
-➠Uptime↠ ${uptimeString}
-
-﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
-Thanks for using ↠ \n${botName}
-﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏`,
-      attachment: await global.utils.getStreamFromURL(link)
-    });
+    message.reply({ 
+      body: `${name}, here is the information 🌝
+🌸 Bot's Name: ${a}
+🌸 Bot's prefix: ${b}  
+🌸 Owner: ${c}
+🌸 Gender: ${e}
+🌸 Messenger: ${d}
+🌸 Insta: ${f}
+🌸 Relationship: ${g}`,
+mentions: ment,
+      attachment: attachment });
   }
 };
